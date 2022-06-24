df31dbbd0f1c8b3ff553e778f1f3b3dc1157361a
palch <palchuk.volodymyr@gmail.com>
2022-06-24 20:10:38 +0400

    network

---
+++ app/src/main/java/com/vladimir/cryptocurrency/api/ApiService.kt
@@ -0,0 +1,39 @@
+package com.vladimir.cryptocurrency.api
+
+import com.vladimir.cryptocurrency.pojo.CoinClassInfoData
+import com.vladimir.cryptocurrency.pojo.CoinInfoListOfData
+import com.vladimir.cryptocurrency.pojo.CoinPriceInfo
+import io.reactivex.Single
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ApiService {
+
+    @GET("top/totalvolfull")
+    fun getTopCoinsInfo(
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = "",
+        @Query(QUERY_PARAM_LIMIT) limit: Int = 10,
+        @Query(QUERY_PARAM_TO_SYMBOL) tSym: String = CURRENCY
+
+    ) : Single<CoinInfoListOfData>
+
+
+    @GET("pricemultifull")
+    fun getFullPRiceList(
+        @Query(QUERY_PARAM_API_KEY) apiKey: String = "",
+        @Query(QUERY_PARAM_FROM_SYMBOLS) fSyms: String,
+        @Query(QUERY_PARAM_TO_SYMBOLS) tSyms: String = CURRENCY
+
+    ) : Single<CoinClassInfoData>
+
+
+    companion object {
+        private const val QUERY_PARAM_API_KEY = "api_key"
+        private const val QUERY_PARAM_LIMIT = "limit"
+        private const val QUERY_PARAM_TO_SYMBOL = "tsym"
+        private const val QUERY_PARAM_TO_SYMBOLS = "tsyms"
+        private const val QUERY_PARAM_FROM_SYMBOLS = "fsyms"
+
+        private const val CURRENCY = "USD"
+    }
+}
